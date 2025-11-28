cdbd0bd7a5e1b64719fb959188cf74089178ebff
Flávio R. Troszczanczuk <flavioczuk@gmail.com>
2025-11-28 00:55:42 -0300

    fixing build

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,9 +2,6 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 
-// components
-import { Form, Input, InputHidden, Submit, Textarea, Select, Checkbox } from '@/components/Form'
-
 // svg
 import UxPhone from '@/assets/svg/ux/phone.svg'
 import UxEmail from '@/assets/svg/ux/envelope.svg'
